9f4eef61bfec7b69663725e1a98e05ee66f30669
ezedin ebrahim <ezedinebrahim131@gmail.com>
2025-09-06 08:08:17 +0300

    com2309

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
-    // Static export requires unoptimized images since there's no Image Optimization server
-    unoptimized: true,
   },
-  // Generate a static export in the `out/` directory
-  output: 'export',
 }
 
 module.exports = nextConfig
