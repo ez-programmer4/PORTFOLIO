3425ee14f148bceedeb1712c5f560b5c0c6523bf
ezedin ebrahim <ezedinebrahim131@gmail.com>
2025-09-06 08:05:25 +0300

    com230

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com'],
     // Static export requires unoptimized images since there's no Image Optimization server
